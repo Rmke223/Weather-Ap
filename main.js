a05c612f3621f2de68ca77103956416ff726c0c4
Rmke223 <70180189+Rmke223@users.noreply.github.com>
2020-10-02 09:32:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 var zip = ""
 var month = new Date();
+//gets the current month
 var numMonth = (month.getMonth()+1)
 
+//hides the content section on load
 document.getElementById("content").style.visibility = "hidden"
 
-
-
-
-
-
-
-
-
-
-
-
+//decides the image to display based on the curent month
 if(numMonth == 9 || numMonth == 10 || numMonth == 11){
     document.getElementById("season").src = "./img/fall.jpg";
 }
@@ -32,21 +24,25 @@ else{
 
 
 
-
+//calling API 
 function getZip() {
+  //assigning variable to user input so it can be used in ther
     var userInput = document.getElementById("userInput").value;
     zip = "https://api.openweathermap.org/data/2.5/weather?zip="+userInput+"&appid=d02c4a194c15c23d8256f9f384c418a3"
 
 fetch(zip)
 .then((resp) => resp.json()) // Transform the data into json
 .then(function(data) {
-  console.log(data)
+  //displays data from API
   document.getElementById("content").style.visibility = "initial"
   document.getElementById("cityName").innerHTML = data.name
   document.getElementById("kelv").innerHTML = (Math.round(data.main.temp)) + " K"
   document.getElementById("far").innerHTML = Math.round(((data.main.temp)*(9/5)-459.67)) + " F"
   document.getElementById("cels").innerHTML = Math.round((data.main.temp)-273.15) + " C"
   document.getElementById("condName").innerHTML = data.weather[0].description
+  
+  
+  //displays icon based on condition code
   var iconVal = data.weather[0].id
   if(iconVal < 600 && iconVal > 499){
     document.getElementById("icon").className = "fas fa-cloud-showers-heavy";
@@ -68,13 +64,13 @@ fetch(zip)
   }
   
   })
+
+
+  //When there is no info sent back from API, dipslay error prompt and hide content
   .catch(function(error) {
     alert("You have entered an invalid Zip!");
-    document.getElementById("cityName").innerHTML = "N/A"
-    document.getElementById("kelv").innerHTML = "N/A"
-    document.getElementById("far").innerHTML = "N/A"
-    document.getElementById("cels").innerHTML = "N/A"
-    document.getElementById("condName").innerHTML = "N/A"
+  
     document.getElementById("content").style.visibility = "hidden"
     });
 }
+
